ca39a92a2991174a043cda72ba2580f8e3c66470
Parkpoom.noikorn <poom_nura@hotmail.com>
2022-04-04 13:25:31 +0700

    Publish To GitHub
    Publish To GitHub

---
+++ WriteLogErrorToText/cSetLog.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO; 
+using System.Text; 
+
+namespace WriteLogToText
+{
+    public class cSetLog
+    {
+        public static bool C_PRCbWriteLog(string ptErrorLog1)
+        {
+            StringBuilder oSql;
+            string tDteTme;
+            try
+            {
+                oSql = new StringBuilder();
+                tDteTme = DateTime.Now.ToString();
+                oSql.Append("Error log is : " + ptErrorLog1 + " Time : " + tDteTme);
+                File.AppendAllText(Environment.SpecialFolder.ProgramFiles + "log.txt", oSql.ToString());
+                oSql.Clear();
+                return true;
+            }
+            catch (Exception oEx)
+            {
+                return false;
+            }
+
+
+        } 
+
+    }
+}
+
